020b2ba246904ebd4278b170d5378eaac9844748
Manish Mandal <156222013+maneeish@users.noreply.github.com>
2025-05-18 19:11:07 +0530

    Update Day_9_TypeCasting.py

--- Day_9_TypeCasting.py
+++ Day_9_TypeCasting.py
@@ -1,1 +1,18 @@
+#Implicit typecasting
+a = 10        # int
+b = 2.5       # float
+
+result = a + b
+print(result)        # 12.5
+print(type(result))  # <class 'float'>
+
+#Explicit Typecasting
+x = "100"
+y = int(x)     # string to int
+print(y + 50)  # 150
+
+a = 5
+b = str(a)     # int to string
+print(b + "0") # "50"
+
 
