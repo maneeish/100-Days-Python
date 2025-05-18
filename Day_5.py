50f7f86f2fab44ced5679866ecf99f6fc97738fb
Manish Mandal <156222013+maneeish@users.noreply.github.com>
2025-05-18 17:39:55 +0530

    Create Day_5.py

---
+++ Day_5.py
@@ -0,0 +1,24 @@
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+#Escape Sequence
+print("This doesnt "execute")
+print("This will \" execute")
