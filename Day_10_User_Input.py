6316d7d6ac49f07132f5aa7e978eaef428b5d16a
Manish Mandal <156222013+maneeish@users.noreply.github.com>
2025-05-18 19:27:08 +0530

    Create Day_10_User_Input.py

---
+++ Day_10_User_Input.py
@@ -0,0 +1,27 @@
+a=input()
+print("My Name is:",a)
+
+
+#Typecasting is needed
+x=input("Enter First Number:")
+y=input("Enter Second Number:")
+print(x+y)
+
+#Using TypeCasting
+
+a=int(input("Enter First Number:"))
+b=int(input("Enter Second Number:"))
+print(a+b)
+
+
+#Output
+Manish
+My Name is: Manish
+Enter First Number:5
+Enter Second Number:6
+56
+Enter First Number:5
+Enter Second Number:6
+11
+
+=== Code Execution Successful ===
